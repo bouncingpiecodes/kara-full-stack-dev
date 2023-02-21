6dce3da6997ac5ffdfe042e1e1b0e016a460ccf1
“kara <bouncingpie@icloud.com>
2023-02-21 00:35:40 -0500

    adding my resume and skills

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,1 +1,35 @@
 import React from "react";
+
+export default function Resume() {
+  return (
+    <div className="m-5 mt-0 mb-0 d-flex flex-column align-items-center align-items-lg-start">
+      <h2 className="text-center text-lg-start">Resume</h2>
+      <br></br>
+      <h5>
+        <a href="file:///Users/kara/Documents/Kara%20H.%20Resume%20-%20Google%20Docs.html">
+          Resume
+        </a>
+      </h5>
+      <br></br>
+      <div className="d-flex flex-column flex-lg-row container row">
+        <div className="d-flex flex-column col-6">
+          <h4>Web Dev Skills</h4>
+          <ul>
+            <lil>fill in later</lil>
+          </ul>
+        </div>
+        <div className="d-flex flex-column col-6">
+          <h4>coding languages</h4>
+          <ul className="col-9">
+            <li>Javascript</li>
+            <li>SQL</li>
+          </ul>
+          <h4>Certifications</h4>
+          <ul className="col-9">
+            <li>GA Tech FullStack coding bootcamp</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
